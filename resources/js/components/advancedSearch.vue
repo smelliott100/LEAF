050dccaf6eef4ef61a23e73532505699a6e16955
jeanmarcablevets <jeanmarc.daviau@ablevets.com>
2019-05-21 11:41:41 -0400

    addressed feedback

--- resources/js/components/advancedSearch.vue
+++ resources/js/components/advancedSearch.vue
@@ -11,15 +11,15 @@
             <div class="clearfix"><button title="Close" @click="toggle" class="closeSearch float-right p-1"><i class="fas fa-times close-button-lg"></i></button></div>
             <div v-for="(filter, index) in filters" class="row">
                 <div class="w-10 px-2"><button title="Remove filter" v-if="filters.length > 1" @click="removeFilter(index)" class="removeFilter"><i class="fas fa-minus"></i></button></div>
-                <div class="w-10 px-2"><b-form-select title="Please select a type to filter" v-model="filter.type" :options="filterTypeOptions"></b-form-select></div>
-                <div class="w-25 px-2" v-if="filter.type === 'inbox'"><b-form-select title="Please select an inbox field" v-model="filter.field" :options="inboxFieldOptions"></b-form-select></div>
-                <div class="w-25 px-2" v-else-if="filter.type === 'request'"><b-form-select title="Please select a request filter" v-model="filter.field" :options="requestFieldOptions"></b-form-select></div>
-                <div class="w-25 px-2 mt-3 ml-3" v-else><span class="text-center">Please select a type first</span></div>
+                <div class="w-15 px-2"><b-form-select title="Please select a type to filter" v-model="filter.type" :options="filterTypeOptions"></b-form-select></div>
+                <div class="w-15 px-2" v-if="filter.type === 'inbox'"><b-form-select title="Please select an inbox field" v-model="filter.field" :options="inboxFieldOptions"></b-form-select></div>
+                <div class="w-15 px-2" v-else-if="filter.type === 'request'"><b-form-select title="Please select a request filter" v-model="filter.field" :options="requestFieldOptions"></b-form-select></div>
+                <div class="w-15 px-2" v-else><select title="Please select a type to filter first"><option>Please select a type first</option></select></div>
                 <div class="w-25 px-2"><b-form-select title="Please select an operator" v-model="filter.operator" :options="operatorOptions"></b-form-select></div>
-                <div class="w-25 px-2"><b-form-input title="Please enter a parameter" v-model="filter.data"></b-form-input></div>
+                <div class="w-25 px-2"><b-form-input placeholder="Please enter a parameter" title="Please enter a parameter" v-model="filter.data"></b-form-input></div>
             </div>
-            <div class="ml-lg-8"><button title="Add new filter" @click="newFilter" class="operatorAnd p-2 mx-sm-0 mx-md-0">AND</button></div>
-            <div class="search"><button @click="applyFilter">Search</button></div>
+            <div class="ml-lg-8"><button title="Add new filter" @click="newFilter" class="operatorAnd py-3 px-4 mx-sm-0 mx-md-0">AND</button></div>
+            <div class="search search-lg"><button class="p-3" @click="applyFilter">Search</button></div>
         </div>
     </div>
 </template>
@@ -43,20 +43,20 @@
                     data: null
                 }],
                 inboxFieldOptions: [
-                    { value: null, text: '' }
+                    { value: null, text: 'fields' }
                 ],
                 requestFieldOptions: [
-                    { value: null, text: '' }
+                    { value: null, text: 'fields' }
                 ],
                 operatorOptions: [
-                    { value: null, text: '' },
+                    { value: null, text: 'operators' },
                     { value: 'equals', text: '=' },
                     { value: 'contains', text: 'Contains' },
                     { value: 'greaterThan', text: '>' },
                     { value: 'lessThan', text: '<' }
                 ],
                 filterTypeOptions: [
-                    { value: null, text: '' },
+                    { value: null, text: 'types' },
                     { value: 'inbox', text: 'Inbox' },
                     { value: 'request', text: 'Requests' }
                 ]
