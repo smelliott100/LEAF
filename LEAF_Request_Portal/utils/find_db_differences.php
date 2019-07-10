abf16a38ba00a0a685a53e81331a7428acc6ade1
Patrick Liesveld <pliesveld@users.noreply.github.com>
2019-07-10 13:09:42 -0400

    updated find script to reduce false positives

--- LEAF_Request_Portal/utils/find_db_differences.php
+++ LEAF_Request_Portal/utils/find_db_differences.php
@@ -1,9 +1,16 @@
 <?php
 
-include '../db_mysql.php';
-include '../db_config.php';
+
+include 'src/db_mysql.php';
+include 'src/db_mysql2.php';
+include 'src/db_config.php';
+include 'src/db_config2.php';
+
 define('LF', "\r\n");
+ini_set('display_errors', 1);
 $db_config = new DB_Config();
+$db_config2 = new DB_Config2();
+
 
 $time_server_brought_offline_for_migration = '';
 
@@ -12,13 +19,25 @@ $firstDB_host = $db_config->dbHost;
 $firstDB_user = $db_config->dbUser;
 $firstDB_pass = $db_config->dbPass;
 
-$secondDB = 'leaf_data';
-$secondDB_host = $db_config->dbHost;
-$secondDB_user = $db_config->dbUser;
-$secondDB_pass = $db_config->dbPass;
 
-$db = new DB($firstDB_host, $firstDB_user, $firstDB_pass, $firstDB);
-$db2 = new DB($secondDB_host, $secondDB_user, $secondDB_pass, $secondDB);
+$secondDB = 'leaf_data';
+$secondDB_host = $db_config2->dbHost;
+$secondDB_user = $db_config2->dbUser;
+$secondDB_pass = $db_config2->dbPass;
+
+#$db = new DB($firstDB_host, $firstDB_user, $firstDB_pass, $firstDB);
+#$db2 = new DB($secondDB_host, $secondDB_user, $secondDB_pass, $secondDB);
+
+$dbs_with_changes = file('./dbs_with_changes.txt');
+foreach($dbs_with_changes as $name) {
+    //$db = new DB($firstDB_host, $firstDB_user, $firstDB_pass, trim($name) . "_on_cloud");
+    $db = new DB($firstDB_host, $firstDB_user, $firstDB_pass, trim($name));
+    $db2 = new DB2($secondDB_host, $secondDB_user, $secondDB_pass, trim($name));
+    $firstDB = trim($name);
+    findRecords();
+}
+
+
 
 // Return FALSE if no diff, otherwise show num diff
 function isResDiff($arr1, $arr2) {
@@ -31,35 +50,61 @@ function isResDiff($arr1, $arr2) {
     }
 }
 
-$hasDiff = 0;
+function findRecords() {
+  global $db, $db2, $firstDB;
 
-// Check differences in records
-$res = $db->query('SELECT max(date), max(submitted), max(deleted), count(*) FROM records');
-$res2 = $db2->query('SELECT max(date), max(submitted), max(deleted), count(*) FROM records');
+  $hasDiff = 0;
 
-if(isResDiff($res, $res2)) {
-    echo $firstDB . LF;
-    $hasDiff = 1;
+  // Check differences in records
+ //$res = $db->query('SELECT max(date), max(submitted), max(deleted), count(*) FROM records');
+ //$res2 = $db2->query('SELECT max(date), max(submitted), max(deleted), count(*) FROM records');
+//  if(isResDiff($res, $res2)) {
+//      echo $firstDB . LF;
+//      $hasDiff = 1;
+//  }
+
+
+  $res = $db->query('SELECT recordID, userID, date FROM records WHERE recordID = (SELECT MAX(recordID) FROM records)');
+  $targetID = $res[0]['recordID'];
+  //echo "Checking {$targetID}" . LF;
+
+  $res2 = $db2->query('SELECT recordID, userID, date FROM records WHERE recordID = ' . $targetID);
+
+
+  //echo "Checking src" . LF;
+  //var_dump($res[0]);
+  //echo "Checking dest" . LF;
+  //var_dump($res2[0]);
+
+  if($res[0]['userID'] != $res2[0]['userID']) {
+     echo $res[0]['userID'] . " != " . $res2[0]['userID'] . LF;
+     echo $firstDB . LF;
+     $hasDiff = 1;
+  } else if($res[0]['date'] != $res2[0]['date']) {
+     echo $res[0]['date'] . " != " . $res2[0]['date'] . LF;
+     echo $firstDB . LF;
+     $hasDiff = 1;
+  }
+   
+  /*
+  for ($i = $res[0]['recordID']; $i >= 0 ; $i--) {
+ //     echo "Checking {$i}\n";
+      $res2 = $db2->query('SELECT recordID, userID, date FROM records WHERE recordID = ' . $targetID);
+
+      //var_dump($res2[0]);
+
+      if($res[0]['userID'] != $res2[0]['userID']) {
+        echo $res[0]['userID'] . " != " . $res2[0]['userID'];
+        echo $firstDB . LF;
+        $hasDiff = 1;
+      }
+     
+      if($res[0]['date'] != $res2[0]['date']) {
+        echo $res[0]['date'] . " != " . $res2[0]['date'];
+        echo $firstDB . LF;
+        $hasDiff = 1;
+      }
+  }
+   */
 }
 
-// Check differences in action_history
-$res = $db->query('SELECT max(time) FROM action_history');
-$res2 = $db2->query('SELECT max(time) FROM action_history');
-
-if(isResDiff($res, $res2)
-    && $hasDiff == 0
-) {
-    echo $firstDB . LF;
-    $hasDiff = 1;
-}
-
-// Check differences in data
-$res = $db->query('SELECT max(timestamp) FROM `data`');
-$res2 = $db2->query('SELECT max(timestamp) FROM `data`');
-
-if(isResDiff($res, $res2)
-    && $hasDiff == 0
-) {
-    echo $firstDB . LF;
-    $hasDiff = 1;
-}
